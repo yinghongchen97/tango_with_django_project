36c4eeae917d07d9c3df1b338d9122c839b62a87
Yinghong Chen <2488742c@student.gla.ac.uk>
2021-07-28 18:01:16 +0800

    ch04

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Rango says hey there partner!   <a href='/rango/about/'>About</a>")
+# def index(request):
+#     return HttpResponse("Rango says hey there partner!   <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.   <a href='/rango/'>Index</a>")
+    #return HttpResponse("Rango says here is the about page.   <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -25,4 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),
     
-]
+ ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
